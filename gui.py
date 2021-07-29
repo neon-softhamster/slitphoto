7e7533372449b589ba18a2bfc3330a6aef84da4b
neon-softhamster <gpvruss@gmail.com>
2021-07-30 01:52:28 +0700

    fixed slider + faster frame loading

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import sys
 import cv2
+from cv2 import VideoCapture, CAP_PROP_POS_FRAMES, CAP_PROP_FRAME_COUNT, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, \
+    CAP_PROP_FPS
 import os
 from range_slider import QRangeSlider
 
@@ -17,30 +19,34 @@ class MainWindow(QMainWindow, gs.Ui_Window):
         self.setupUi(self)
         self.name_of_file = ["", ""]
 
-        #adding slider
-        self.RS = QRangeSlider()
-        self.slider_layout.addWidget(self.RS)
-
-        shadow_effect = []
-        for i in range(5):
-            shadow_effect.append(QGraphicsDropShadowEffect())
-            shadow_effect[i].setBlurRadius(20)
-            shadow_effect[i].setXOffset(0)
-            shadow_effect[i].setYOffset(5)
-            shadow_effect[i].setColor(QtGui.QColor(40, 40, 40))
+        # generates shadow effects for buttons
+        self.shadow_effect = []
+        for i in range(6):
+            self.shadow_effect.append(QGraphicsDropShadowEffect())
+            self.shadow_effect[i].setBlurRadius(20)
+            self.shadow_effect[i].setXOffset(0)
+            self.shadow_effect[i].setYOffset(5)
+            self.shadow_effect[i].setColor(QtGui.QColor(40, 40, 40))
 
         # add stock img
         self.fst_frame.setPixmap(QPixmap("resources\\fst.jpg"))
         self.lst_frame.setPixmap(QPixmap("resources\\lst.jpg"))
-        self.fst_frame.setGraphicsEffect(shadow_effect[0])
-        self.lst_frame.setGraphicsEffect(shadow_effect[1])
+        self.fst_frame.setGraphicsEffect(self.shadow_effect[0])
+        self.lst_frame.setGraphicsEffect(self.shadow_effect[1])
 
         # adding shadows
-        self.btn_explore_file.setGraphicsEffect(shadow_effect[2])
-        self.render_btn.setGraphicsEffect(shadow_effect[3])
-        self.how_to_btn.setGraphicsEffect(shadow_effect[4])
+        self.btn_explore_file.setGraphicsEffect(self.shadow_effect[2])
+        self.render_btn.setGraphicsEffect(self.shadow_effect[3])
+        self.how_to_btn.setGraphicsEffect(self.shadow_effect[4])
 
-        ### ACTIONS HERE ###
+        # adding slider
+        self.RS = QRangeSlider()
+
+        # creates CV2 video, so it can be reached from any func or meth of program. Also no need to read file every
+        # time you want to change frame
+        self.video_f = VideoCapture()
+
+        # ACTIONS HERE #
         # button action of searching video file
         self.btn_explore_file.clicked.connect(self.search_name_file)
 
@@ -51,71 +57,43 @@ class MainWindow(QMainWindow, gs.Ui_Window):
     def search_name_file(self):
         self.name_of_file = QFileDialog.getOpenFileName(self, "Open video file", os.getcwd(), "Video files (*.mp4)")
         if self.name_of_file[0] != "":
-            frame_in_window = [FrameInWindow(self.name_of_file[0], 0, 320, "fst"),
-                               FrameInWindow(self.name_of_file[0], 0, 320, "lst")]  # path to file, number of frame & h
+            self.video_f.release()
+            self.video_f.open(self.name_of_file[0])
 
-            self.set_frames_to_grid(frame_in_window)
+            # adding range slider RS to layout
+            self.slider_layout.addWidget(self.RS)
 
             # update slider limits
-            # self.RS.setRange(0, int(frame_in_window[0].video_file.get_video_info()[2]))
-
-            # clean up all frames
-            for i in frame_in_window:
-                i.__del__()
+            self.RS.setMin(0)
+            self.RS.setMax(int(self.video_f.get(CAP_PROP_FRAME_COUNT)))
+            self.RS.setStart(50)
+            self.RS.setEnd(self.video_f.get(CAP_PROP_FRAME_COUNT) - 50)
+            self.set_frames_to_grid(self.RS.getRange()[0], self.RS.getRange()[1])
         else:
             pass
 
-    def set_frames_to_grid(self, frame_in_window):
-        self.fst_frame.setPixmap(frame_in_window[0].qt_img)
-        self.lst_frame.setPixmap(frame_in_window[1].qt_img)
+    def set_frames_to_grid(self, fst_n, lst_n):
+        self.fst_frame.setPixmap(self.convert_cv2qt(320, fst_n))
+        self.lst_frame.setPixmap(self.convert_cv2qt(320, lst_n))
 
         # add frames and text to grid
         self.grid.addWidget(self.fst_frame, 0, 0)
         self.grid.addWidget(self.lst_frame, 0, 1)
 
-        # set up selectors limits
-
     def select_frame(self):
-        if self.name_of_file[0] != "":
-            print(round(self.RS.getRange()[0]), round(self.RS.getRange()[1]))
-            frame_in_window = [FrameInWindow(self.name_of_file[0], self.RS.getRange()[0],
-                                             320, "fst"),
-                               FrameInWindow(self.name_of_file[0], self.RS.getRange()[1],
-                                             320, "lst")]
+        self.set_frames_to_grid(self.RS.getRange()[0], self.RS.getRange()[1])
 
-            self.set_frames_to_grid(frame_in_window)
-
-            # clean up all frames
-            for i in frame_in_window:
-                i.__del__()
-        else:
-            pass
-
-
-class FrameInWindow:
-    def __init__(self, src, frame_n, h, numb):
-        self.frame_n = frame_n
-        self.h = h
-        self.number = numb
-
-        # open video_file for usage
-        self.video_file = cs.VideoFile(src)
-        self.aspect_ratio = self.video_file.get_video_info()[0] / self.video_file.get_video_info()[1]
-
-        # convert the image to Qt format
-        self.qt_img = self._convert_cv_qt(self.video_file.get_special_frame(self.frame_n))
-
-    def _convert_cv_qt(self, cv_img):
-        # Convert from an opencv image to QPixmap
+    # Convert from an opencv image to QPixmap (necessary to add pic to qt layout)
+    def convert_cv2qt(self, height, n):  # input is height (ip pixels), that will be in window, n - nb of frame
+        self.video_f.set(CAP_PROP_POS_FRAMES, n)    # go to n frame
+        inf, cv_img = self.video_f.read()   # reading frame to cv pic
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_qt_format.scaled(round(self.aspect_ratio * self.h), self.h, Qt.KeepAspectRatio)
+        aspect_ratio = self.video_f.get(CAP_PROP_FRAME_WIDTH) / self.video_f.get(CAP_PROP_FRAME_HEIGHT)
+        p = convert_to_qt_format.scaled(round(aspect_ratio * height), height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
-
-    def __del__(self):
-        self.video_file.__del__()
 
 
 if __name__ == "__main__":
@@ -125,7 +103,7 @@ if __name__ == "__main__":
 
     with open("style.qss", "r") as s:
         style = s.read()
-    
+
     app.setStyleSheet(style)
     mw = MainWindow()
     mw.show()
